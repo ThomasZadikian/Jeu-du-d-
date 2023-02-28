d348ccd5f46bf3d94f461caed927bf6db20ea991
Thomas <Thoma.zadikian@gmail.com>
2023-02-28 10:02:04 +0100

    insert dice img and associate with random number

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 let count = document.getElementById("count");
-let roll = document.getElementById("roll");
+let roll = document.getElementById("roll-button");
 let numberRandom = document.getElementById("number");
 let player1 = document.getElementById("player1");
 let player2 = document.getElementById("player2");
 let save = document.getElementById("save");
 let totalPlayer1 = document.getElementById("totalPlayer1");
+let dice = document.getElementById("dice");
 
 totalCount = 0;
 actualCount = 0;
@@ -17,6 +18,8 @@ function randomNumber(min, max) {
 
 function player1Count() {
   let numb = randomNumber(1, 6);
+  const diceImage = "../images/dice" + numb + ".png";
+  document.querySelectorAll("img")[0].setAttribute("src", diceImage);
   numberRandom.innerHTML = numb;
   if (numb === 1) {
     actualCount = 0;
